d1e50a0910ef9e2d4436de3a4910418296525267
Inceptiqn <aceacewastaken@gmail.com>
2024-01-24 18:56:19 +0100

    first commit

---
+++ dwm/themes/monokai.h
@@ -0,0 +1,6 @@
+static const char col_gray1[] = "#272822";
+static const char col_gray2[] = "#49483e";
+static const char col_gray3[] = "#a1efe4";
+static const char col_gray4[] = "#383830";
+static const char col_gray5[] = "#a6e22e";
+static const char col_cyan[] = "#a6e22e";
